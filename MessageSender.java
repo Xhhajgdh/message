29430d9d98de0de0b0e3e0ee9b9cb63a50ff08f1
Xhhajgdh <1913145200@qq.com>
2023-11-13 20:37:22 +0800

    首次提交

---
+++ MessageSender.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class MessageSender {
+
+    public static void main(String[] args) {
+        MessageService service = new MessageService();
+        service.initializeService();
+
+        List<String> phoneNumbers = Arrays.asList(
+            "+1234567890",
+            "+1987654321",
+            "+1123456789"
+        );
+
+        MessageContent messageContent = new MessageContent("Hello, this is a test message!");
+
+        for (String phoneNumber : phoneNumbers) {
+            service.sendMessage(phoneNumber, messageContent.getMessage());
+        }
+    }
+}
---
+++ MessageService.java
@@ -0,0 +1,12 @@
+public class MessageService {
+
+    public void initializeService() {
+        System.out.println("Initializing messaging service...");
+        // 在这里初始化短信服务，例如设置API密钥等
+    }
+
+    public void sendMessage(String phoneNumber, String message) {
+        System.out.println("Sending message to " + phoneNumber + ": " + message);
+        // 这里添加发送短信的代码
+    }
+}
